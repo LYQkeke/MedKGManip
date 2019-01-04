fee6e4faf2623f4508b8393c1ec179c9b24eaf9b
KEKE <493006007@qq.com>
2019-01-04 16:03:15 +0800

    add pom architecture

---
+++ src/main/java/com/keke/Main.java
@@ -0,0 +1,13 @@
+package com.keke;
+
+/**
+ * Created by KEKE on 2019/1/4
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("this is Main class");
+
+    }
+}
